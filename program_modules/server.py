ed4b2d70fa66cf8451ed03492969cae38ab60be9
vadila234git <155548075+vadila234git@users.noreply.github.com>
2024-11-24 19:56:32 +0200

    some changes

--- program_modules/server.py
+++ program_modules/server.py
@@ -2,17 +2,21 @@ import socket
 
 def start_server():
     with socket.socket(family = socket.AF_INET, type = socket.SOCK_STREAM) as server_socket:
-        server_socket.bind(("0.0.0.0", 22))
+        server_socket.bind(("192.168.1.14", 2232))
 
         server_socket.listen(2)
         print("connecting...")
 
         client_socket, adress = server_socket.accept()
         print("connected:", adress)
-        client_socket.sendall("1".encode("utf-8"))
+
+        client_socket.sendall("you connected".encode("utf-8"))
+        
         client_socket2, adress2 = server_socket.accept()
         print("connected:", adress2)
-        client_socket2.sendall("2".encode("utf-8"))
+
+        client_socket2.sendall("you connected".encode("utf-8"))
+
         while True:
             data = client_socket.recv(1024)
             
