8aadc7bdb935a8aa7fe6fed11d0768982ec3d5e5
vadila234git <155548075+vadila234git@users.noreply.github.com>
2024-11-26 17:25:32 +0200

    som changes

--- program_modules/server.py
+++ program_modules/server.py
@@ -1,33 +1,35 @@
 import socket
 
 def start_server():
-    with socket.socket(family = socket.AF_INET, type = socket.SOCK_STREAM) as server_socket:
-        server_socket.bind(("192.168.1.14", 2232))
+    try:
+        with socket.socket(family = socket.AF_INET, type = socket.SOCK_STREAM) as server_socket:
+            server_socket.bind(("192.168.1.14", 2232))
 
-        server_socket.listen(2)
-        print("connecting...")
+            server_socket.listen(2)
+            print("connecting...")
 
-        client_socket, adress = server_socket.accept()
-        print("connected:", adress)
+            client_socket, adress = server_socket.accept()
+            print("connected:", adress)
 
-        client_socket.sendall("you connected".encode("utf-8"))
-        
-        client_socket2, adress2 = server_socket.accept()
-        print("connected:", adress2)
-
-        client_socket2.sendall("you connected".encode("utf-8"))
-
-        while True:
-            data = client_socket.recv(1024)
+            client_socket.sendall("1".encode("utf-8"))
             
-            if data:
-                print("send data to client 2")
-                client_socket2.sendall(data)
+            client_socket2, adress2 = server_socket.accept()
+            print("connected:", adress2)
 
-            data2 = client_socket2.recv(1024)
+            client_socket2.sendall("2".encode("utf-8"))
 
-            if data2:
-                print("send data to client 1")
-                client_socket.sendall(data2)
+            while True:
+                data = client_socket.recv(1024)
+                
+                if data:
+                    client_socket2.sendall(data)
+
+                data2 = client_socket2.recv(1024)
+
+                if data2:
+                    client_socket.sendall(data2)
+
+    except:
+        print("end of work")
                
 start_server()
