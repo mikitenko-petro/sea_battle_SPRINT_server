64f72c3439e5641347eb245cae9c3cdac76ad09a
vadila234git <155548075+vadila234git@users.noreply.github.com>
2024-12-28 23:19:00 +0200

    version 0.0.1

--- program_modules/server.py
+++ program_modules/server.py
@@ -1,33 +1,34 @@
 import socket
 
 def start_server():
-    try:
-        with socket.socket(family = socket.AF_INET, type = socket.SOCK_STREAM) as server_socket:
-            IP = socket.gethostbyname(socket.gethostname())
-            server_socket.bind((IP, 2232))
-            print(IP)
+    while True:
+        try:
+            with socket.socket(family = socket.AF_INET, type = socket.SOCK_STREAM) as server_socket:
+                IP = socket.gethostbyname(socket.gethostname())
+                server_socket.bind((IP, 2232))
+                print(IP)
 
-            server_socket.listen()
-            print("connecting...")
+                server_socket.listen()
+                print("connecting...")
 
-            client_socket, adress = server_socket.accept()
-            print("connected:", adress)
+                client_socket, adress = server_socket.accept()
+                print("connected:", adress)
 
-            client_socket.sendall("1".encode("utf-8"))
-            
-            client_socket2, adress2 = server_socket.accept()
-            print("connected:", adress2)
+                client_socket.sendall("1".encode("utf-8"))
+                
+                client_socket2, adress2 = server_socket.accept()
+                print("connected:", adress2)
 
-            client_socket2.sendall("2".encode("utf-8"))
+                client_socket2.sendall("2".encode("utf-8"))
 
-            while True:
-                data = client_socket.recv(1024)
-                if data:
-                    client_socket2.sendall(data)
+                while True:
+                    data = client_socket.recv(1024)
+                    if data:
+                        client_socket2.sendall(data)
 
-                data2 = client_socket2.recv(1024)
-                if data2:
-                    client_socket.sendall(data2)
+                    data2 = client_socket2.recv(1024)
+                    if data2:
+                        client_socket.sendall(data2)
 
-    except Exception as error:
-        print(error)
+        except Exception as error:
+            print(error)
